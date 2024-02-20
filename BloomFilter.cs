301fdc3a79e3275e13333553a715515f3e44010d
Ori Chason <orichason@gmail.com>
2024-02-19 16:01:13 -0800

    Add project files.

---
+++ BloomFilter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloomFilter
+{
+    internal class BloomFilter<T>
+    {
+        int cap;
+        byte[] filter;
+        HashSet<Func<T, int>> hashFunctions;
+        public BloomFilter(int cap, HashSet<Func<T, int>> hashFunctions)
+        {
+            this.cap = cap;
+            filter = new byte[cap];
+            this.hashFunctions = hashFunctions;
+        }
+
+        public void Insert(T item)
+        {
+            foreach (var func in hashFunctions)
+            {
+                int hashCode = func.Invoke(item);
+                int position = hashCode / 2;
+                int byteIndex = hashCode % 8;
+
+                //TODO: bitwise shift to change bits inside byte 
+            }
+        }
+
+        public bool ProbablyContains(T item)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace BloomFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> hashOne = (item) => item.GetHashCode();
+            Func<string, int> hashTwo = (item) => item.GetHashCode() + 5;
+            Func<string, int> hashThree = (item) => item.GetHashCode() * 3 - 21;
+
+            HashSet<Func<string, int>> hashFunctions = new() { hashOne, hashTwo, hashThree };
+
+            BloomFilter<string> bloomFilter = new(40, hashFunctions);
+        }
+    }
+}
